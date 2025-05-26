34385988fabc0ca5a87ea34ca2170e360cd00b85
Ioana <ioana.ciangau@siemens.com>
2025-05-26 17:16:47 +0300

    Add CORS middleware to enable cross-origin requests

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
-import chatRouter from "./src/routes/chat"
+import cors from "cors"; 
+import chatRouter from "./src/routes/chat";
+
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
