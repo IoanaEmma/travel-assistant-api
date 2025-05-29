f1eb8cb586e5ae62b0253bd5f261d8f11212e190
Ioana <ioana.ciangau@siemens.com>
2025-05-29 16:44:42 +0300

    Refactor flight search function to return sorted flight itineraries and improve data parsing

--- src/services/flightService.ts
+++ src/services/flightService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import config from "../config"
-import { FlightsApiResponse } from "../types/flights";
+import { FlightItinerary } from "../types/flights";
+import parser from "../utils/parser";
+import { sortItinerariesByStops } from "../utils/helper";
 
 export async function searchFlights({
     origin,
@@ -16,14 +18,26 @@ export async function searchFlights({
     returnDate?: string;
     passengers: number;
     cabinClass: string;
-}): Promise<FlightsApiResponse> {
-    console.log(`Searching flights from ${origin} to ${destination} on ${departureDate} with ${passengers} passengers in ${cabinClass} class.`);
-    const url = `${config.FLIGHT_API}/roundtrip/${config.FLIGHT_API_KEY}/${origin}/${destination}/${departureDate}/${returnDate}/${passengers}/0/0/${cabinClass}`;
+}): Promise<FlightItinerary[]> {
 
-    const response = await axios.get(url);
-    return {
-        origin,
-        destination,
-        ...response.data
-    };
+    console.log(`Searching flights from ${origin} to ${destination} on ${departureDate} 
+        with ${passengers} passengers in ${cabinClass} class.`);
+
+    try {
+        const url = `${config.FLIGHT_API}/roundtrip/${config.FLIGHT_API_KEY}/${origin}/${destination}/${departureDate}/${returnDate}/${passengers}/0/0/${cabinClass}`;
+        const response = await axios.get(url);
+        const parsedFlights = parser.parseFlightData({
+            origin,
+            destination,
+            ...response.data
+        });
+
+        const sortedFlights = sortItinerariesByStops(parsedFlights);
+
+        return sortedFlights;
+    } catch (error) {
+        console.error(`Error fetching flights: ${error}`);
+        throw error;
+    }
+
 }
